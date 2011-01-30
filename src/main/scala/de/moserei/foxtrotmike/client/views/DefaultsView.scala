11fbcaaf98e9fec27c4babcae1b923915485ecda
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2011-01-30 10:09:23 +0100

    ComboBoxModel observes Repositories

--- src/main/scala/de/moserei/foxtrotmike/client/views/DefaultsView.scala
+++ src/main/scala/de/moserei/foxtrotmike/client/views/DefaultsView.scala
@@ -12,18 +12,38 @@ class MyComboBox[T](model : ComboBoxModel) extends ComboBox[T](Nil) {
 }
 
 //TODO should depend on BaseEntityRepository#all (reload often, see DefaultAutoCompleterModel)
-class MyComboBoxModel[T](a : Seq[T]) extends DefaultComboBoxModel(new java.util.Vector(a.asJava))
+class MyComboBoxModel[T <: BaseModel](a : BaseEntityRepository[T]) extends DefaultComboBoxModel(new java.util.Vector(a.all.asJava)) with Observer {
+  def created(m : BaseModel) = {
+    m match {
+      case f : T => {
+        addElement(f)
+      }
+      case _ => {}
+    }
+  }
+
+  def updated(m : BaseModel) = {}
+  
+  def removed(m : BaseModel) {
+   m match {
+      case f : T => {
+        removeElement(f)
+      }
+      case _ => {}
+    }
+  }
+}
 
 class DefaultsView extends MigPanel("") { 
   val date = new FormattedTextField(DateFormat.getDateInstance(DateFormat.SHORT)) {
     focusLostBehavior = FormattedTextField.FocusLostBehavior.CommitOrRevert
   }
-  val airfield = new MyComboBox[Airfield](new MyComboBoxModel(AllAirfields.all))
-  val towPlane = new MyComboBox[Plane](new MyComboBoxModel(TowPlanes.all))
-  val towPilot = new MyComboBox[Person](new MyComboBoxModel(AllPeople.all))
-  val wireLauncher = new MyComboBox[WireLauncher](new MyComboBoxModel(AllWireLaunchers.all))
-  val operator = new MyComboBox[Person](new MyComboBoxModel(AllPeople.all))
-  val controller = new MyComboBox[Person](new MyComboBoxModel(AllPeople.all))
+  val airfield = new MyComboBox[Airfield](new MyComboBoxModel(AllAirfields))
+  val towPlane = new MyComboBox[Plane](new MyComboBoxModel(TowPlanes))
+  val towPilot = new MyComboBox[Person](new MyComboBoxModel(AllPeople))
+  val wireLauncher = new MyComboBox[WireLauncher](new MyComboBoxModel(AllWireLaunchers))
+  val operator = new MyComboBox[Person](new MyComboBoxModel(AllPeople))
+  val controller = new MyComboBox[Person](new MyComboBoxModel(AllPeople))
   
   add(new Label(I18n("departure_date")))
   add(new Label(I18n("airfield")))
