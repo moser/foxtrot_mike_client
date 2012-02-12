c3a81b60fc28e10dc14bdd336b51a022590a9894
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-02-12 17:20:41 +0100

    Set default time zone of java datetime API to UTC

--- src/main/scala/fmclient/App.scala
+++ src/main/scala/fmclient/App.scala
@@ -3,11 +3,13 @@ package fmclient
 import fmclient.presenters.MainPresenter
 import swing._
 import org.scala_tools.time.Imports._
+import java.util.TimeZone
 
 object App extends SwingApplication {
   private var mainPresenter : MainPresenter = _
-	def startup(args: Array[String]) {
+  def startup(args: Array[String]) {
     DateTimeZone.setDefault(DateTimeZone.UTC)
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel")
     //javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel")
     mainPresenter = new MainPresenter
