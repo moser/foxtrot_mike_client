a792ef22f1566f2f6ffd38c352890e2155e6c89a
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-03-27 15:38:01 +0200

    Made model a property of MyComboBox

--- src/main/scala/fmclient/views/MyComboBox.scala
+++ src/main/scala/fmclient/views/MyComboBox.scala
@@ -3,6 +3,6 @@ package fmclient.views
 import swing._
 import javax.swing.{DefaultComboBoxModel, ComboBoxModel, JComboBox}
 
-class MyComboBox[T](model : ComboBoxModel) extends ComboBox[T](Nil) {
+class MyComboBox[T](var model : ComboBoxModel) extends ComboBox[T](Nil) {
   override lazy val peer: JComboBox = new JComboBox(model) with SuperMixin
 }
