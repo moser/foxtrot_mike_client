b780cb147c8b37e83937157f2ce3449be9d2dc6c
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-07-26 14:30:08 +0200

    Only show disabled records when no other are found

--- src/main/scala/fmclient/models/EnabledOnlyAutoCompleterModel.scala
+++ src/main/scala/fmclient/models/EnabledOnlyAutoCompleterModel.scala
@@ -5,11 +5,11 @@ import fmclient.models.repos.EnabledAndDisabledStringIndexedEntityRepository
 import scala.swing.Publisher
 import java.util.regex.Pattern
 
-class EnabledOnlyAutoCompleterModel[T >: Null <: BaseModel[String]](collection : EnabledAndDisabledStringIndexedEntityRepository[T], extract : T => String, options_ : Map[String, Boolean] = Map())extends DefaultAutoCompleterModel[T](collection, extract, options_) {
+class EnabledOnlyAutoCompleterModel[T >: Null <: BaseModel[String]](collection : EnabledAndDisabledStringIndexedEntityRepository[T], extract : T => String, options_ : Map[String, Boolean] = Map()) extends DefaultAutoCompleterModel[T](collection, extract, options_) {
 
   override def extractMatchingFromCollection(p : Pattern) = {
     var r = extractMatching(p, collection.enabled)
-    if(r.length < 3)
+    if(r.length < 1)
       r = r ++ extractMatching(p, collection.disabled)
     r
   }
