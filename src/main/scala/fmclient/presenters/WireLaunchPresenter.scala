bf0ae33ad184dbbe32a023150107bedef0bdc8ec
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-09-11 14:40:01 +0200

    Added balloon to wirelaunch/operator

--- src/main/scala/fmclient/presenters/WireLaunchPresenter.scala
+++ src/main/scala/fmclient/presenters/WireLaunchPresenter.scala
@@ -5,8 +5,7 @@ import fmclient.models.WireLaunch
 import scala.swing.event._
 import java.util.Date
 import swing.Reactor
-
-
+import fmclient.views.AutoCompleter._
 
 class WireLaunchPresenter(view0: WireLaunchView) extends BasePresenter[WireLaunch, WireLaunchView] {
   def this() = this(new WireLaunchView())  
@@ -15,4 +14,37 @@ class WireLaunchPresenter(view0: WireLaunchView) extends BasePresenter[WireLaunc
 
   map((m,v) => m.wireLauncher = v.wireLauncher.selectedOption.get, (m,v) => v.wireLauncher.selectedOption = m.wireLauncher)
   map((m,v) => m.operator = v.operator.selectedOption.get, (m,v) => v.operator.selectedOption = m.operator)
+/*
+  view.wireLauncher.reactions += {
+    case CreateEvent(c, str, old) => {
+      view.wireLauncher.setEnabled(false)
+      val bp = new WireLauncherBalloonPresenter(str, view.wireLauncher)
+      bp.reactions += {
+        case PlaneBalloonPresenter.OkEvent(o) => {
+          view.wireLauncher.selectedOption = o
+        }
+        case PlaneBalloonPresenter.CancelEvent() => {
+          view.wireLauncher.revertLast
+        }
+      }
+    }
+  }
+*/
+  val seatCreate = new Reactor {
+    listenTo(view.operator)
+    reactions += {
+      case CreateEvent(c, str, old) => {
+        c.setEnabled(false)
+        val bp = new PersonBalloonPresenter(str, c)
+        bp.reactions += {
+          case PersonBalloonPresenter.OkEvent(o) => {
+            c.selectedOption = o
+          }
+          case PersonBalloonPresenter.CancelEvent() => {
+            c.revertLast
+          }
+        }
+      }
+    }
+  }
 }
